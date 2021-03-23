b01c2bad0a0d41f84216a3accc8998a6ee7ff809
shin039 <life.syncopation.4003@gmail.com>
2021-03-23 18:10:08 +0900

    add api what access db

---
+++ pages/api/dbinfo.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+import { db }                              from "../../modules/database";
+
+export default async (req: any, res: any) => {
+  const dbs  = db().instance;
+	const data = await dbs.any( "select * from m_staff")
+	res.status(200).json({ data });
+}
